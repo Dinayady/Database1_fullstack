55f83d36a74dfdde0b4cfcb086b9c3d467674197
dm-gilmutdinova <dm.gilmutdinova@gmail.com>
2024-04-22 22:21:04 +0300

    create api and database

---
+++ routes/employees.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../middleware/auth');
+const {
+  all,
+  add,
+  remove,
+  employee,
+  edit,
+} = require('../controllers/employees');
+
+// /api/employees
+router.get('/', auth, all);
+router.get('/:id', auth, employee);
+router.post('/add', auth, add);
+router.post('/remove/:id', auth, remove);
+router.put('/edit/:id', auth, edit);
+
+module.exports = router;
