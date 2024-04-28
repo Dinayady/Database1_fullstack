53374afbd380329f16a9afc486c1ba1ee9ea7127
dm-gilmutdinova <dm.gilmutdinova@gmail.com>
2024-04-28 21:07:53 +0300

    registration form

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -3,15 +3,43 @@ import { Card, Form, Row, Space, Typography } from 'antd';
 import { CustomInput } from '../../components/customInput/CustomInput';
 import { PasInput } from '../../components/passwordInput/pasInput';
 import { CustomBtn } from '../../components/customButton/CustomBtn';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Paths } from '../../paths';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/auth/authSlice';
+import { useState } from 'react';
+import { useRegisterMutation } from '../../app/services/auth';
+import { User } from '@prisma/client';
+import { isErrorMsg } from '../../utils/isErrorMsg';
+import { ErrorMessage } from '../../components/errorMessage/errorMessage';
+
+type RegisterData = Omit<User, 'id' & { confirmPassword: string }>;
 
 export const Register = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [error, setError] = useState('');
+  const [registerUser] = useRegisterMutation();
+
+  const register = async (data: RegisterData) => {
+    try {
+      await registerUser(data).unwrap();
+      navigate('/');
+    } catch (error) {
+      const maybeError = isErrorMsg(error);
+      if (maybeError) {
+        setError(error.data.message);
+      } else {
+        setError('Unknown error');
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align='middle' justify='center'>
         <Card title='Sign Up' style={{ width: '30rem' }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={register}>
             <CustomInput name='name' placeholder='Name...' />
             <CustomInput type='email' name='email' placeholder='Email...' />
             <PasInput name='password' placeholder='Password...' />
@@ -24,6 +52,7 @@ export const Register = () => {
             <Typography.Text>
               Do have an account? <Link to={Paths.login}>Log In</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
